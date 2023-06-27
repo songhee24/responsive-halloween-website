7b4b3eaf9816023a042ba8b74d55515683cbf2e6
songhee24 <akaratel61@gmail.com>
2023-06-27 14:11:04 +0600

    mobile header styling #2

--- assets/js/utils/navHelper.js
+++ assets/js/utils/navHelper.js
@@ -1,33 +1,32 @@
 const links = [
-    { href: '#home', text: 'Home' },
-    { href: '#about', text: 'About' },
-    { href: '#candy', text: 'Candy' },
-    { href: '#new', text: 'New' }
+  { href: "#home", text: "Home" },
+  { href: "#about", text: "About" },
+  { href: "#candy", text: "Candy" },
+  { href: "#new", text: "New" },
 ];
 
 function generateNavLinks(links, parentElementId) {
-    const parentElement = document.getElementById(parentElementId);
+  const parentElement = document.getElementById(parentElementId);
 
-    links.forEach(link => {
-        const liElement = document.createElement('li');
-        liElement.classList.add('nav__item');
+  links.forEach((link) => {
+    const liElement = document.createElement("li");
+    liElement.classList.add("nav__item");
 
-        const aElement = document.createElement('a');
-        aElement.href = link.href;
-        aElement.classList.add('nav__link');
-        aElement.textContent = link.text;
+    const aElement = document.createElement("a");
+    aElement.href = link.href;
+    aElement.classList.add("nav__link");
+    aElement.textContent = link.text;
 
-        liElement.appendChild(aElement);
-        parentElement.appendChild(liElement);
-    });
+    liElement.appendChild(aElement);
+    parentElement.appendChild(liElement);
+  });
 
-    const aSupportElement = document.createElement('a');
-    aSupportElement.classList.add(...['button', 'button--ghost']);
-    aSupportElement.textContent = 'Support'
-    aSupportElement.href = '#';
+  const aSupportElement = document.createElement("a");
+  aSupportElement.classList.add("button", "button--ghost");
+  aSupportElement.textContent = "Support";
+  aSupportElement.href = "#";
 
-    parentElement.appendChild(aSupportElement);
+  parentElement.appendChild(aSupportElement);
 }
 
-generateNavLinks(links, 'nav-ul-menu');
-
+generateNavLinks(links, "nav-ul-menu");
