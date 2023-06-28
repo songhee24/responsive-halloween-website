34df5901d7f8b83810f812426f178e7325d189b4
songhee24 <akaratel61@gmail.com>
2023-06-29 02:26:47 +0600

    method for rendering categories done

--- assets/js/utils/categoryHelper.js
+++ assets/js/utils/categoryHelper.js
@@ -15,3 +15,24 @@ const categories = [
     category__description: "Pick the most stylish witch hats out there.",
   },
 ];
+
+function generateCategory({ section__title, srcImg, category__description }) {
+  const parentElement = document.querySelector(
+    ".container__container.container.grid"
+  );
+  const categoryDataElement = document.createElement("div");
+  categoryDataElement.classList.add("category__data");
+
+  const imgElement = document.createElement("img");
+  imgElement.classList.add("category__img");
+  imgElement.src = srcImg;
+  imgElement.alt = section__title;
+
+  const h3Element = document.createElement("h3");
+  h3Element.classList.add("category__title");
+  h3Element.textContent = section__title;
+
+  const pElement = document.createElement("p");
+  pElement.classList.add("category_description");
+  pElement.textContent = category__description;
+}
