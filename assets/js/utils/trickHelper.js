274e58b609e627d8ac0f319759e5fbcda210d73b
songhee24 <akaratel61@gmail.com>
2023-06-28 12:03:29 +0600

    data for trick or treat

---
+++ assets/js/utils/trickHelper.js
@@ -0,0 +1,32 @@
+const trickOrTreat = [
+  {
+    trick__title: "Toffee",
+    trick__subtitle: "Candy",
+    price: "11.99",
+    imgSrc: "assets/img/trick-treat1-img.png",
+  },
+  {
+    trick__title: "Scarecrow",
+    trick__subtitle: "Accessory",
+    price: "51.99",
+    imgSrc: "assets/img/trick-treat2-img.png",
+  },
+  {
+    trick__title: "Candy Cane",
+    trick__subtitle: "Candy",
+    price: "8.99",
+    imgSrc: "assets/img/trick-treat3-img.png",
+  },
+  {
+    trick__title: "Pumpkin",
+    trick__subtitle: "Accessory",
+    price: "23.99",
+    imgSrc: "assets/img/trick-treat4-img.png",
+  },
+  {
+    trick__title: "Ghost",
+    trick__subtitle: "Accessory",
+    price: "32.99",
+    imgSrc: "assets/img/trick-treat4-img.png",
+  },
+];
