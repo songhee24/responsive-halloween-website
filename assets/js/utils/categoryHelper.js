357e4f2b857944cd8f00d75e1d37659075a50b3a
songhee24 <akaratel61@gmail.com>
2023-06-29 02:20:13 +0600

    categories data

---
+++ assets/js/utils/categoryHelper.js
@@ -0,0 +1,17 @@
+const categories = [
+  {
+    section__title: "Ghosts",
+    srcImg: "assets/img/category1-img.png",
+    category__description: "Choose the ghosts, the scariest there are.",
+  },
+  {
+    section__title: "Pumpkins",
+    srcImg: "assets/img/category1-img.png",
+    category__description: "You look at the scariest pumpkins there is.",
+  },
+  {
+    section__title: "Witch Hat",
+    srcImg: "assets/img/category1-img.png",
+    category__description: "Pick the most stylish witch hats out there.",
+  },
+];
