3894b0047ef7f76bdb6146856b3eaa51c3e31742
songhee24 <akaratel61@gmail.com>
2023-06-30 02:07:48 +0600

    creating helper of  new arrivals

--- assets/js/utils/newArrivalsHelper.js
+++ assets/js/utils/newArrivalsHelper.js
@@ -42,3 +42,39 @@ const newArrivals = [
     discountPrice: "$29.99",
   },
 ];
+
+// <div className="swiper-slide new__content">
+//     <div className="new__tag">New</div>
+//     <img src="assets/img/new1-img.png" alt="new" className="new__img">
+//         <h3 className="new__title">Haunted House</h3>
+//         <span className="new__subtitle">Accessory</span>
+//
+//         <div className="new__prices">
+//             <span className="new__price">#14.99</span>
+//             <span className="new__discount">#29.99</span>
+//         </div>
+//
+//         <button className="button new__button">
+//             <i className="bx bx-cart-alt new__icon"></i>
+//         </button>
+// </div>
+
+function generateNewArrivalCard({
+  new__title,
+  new__subtitle,
+  imgSrs,
+  price,
+  discountPrice,
+}) {
+  const newContentElement = document.createElement("div");
+  newContentElement.classList.add("swiper-slide", "new__content");
+
+  const newTagElement = document.createElement("div");
+  newTagElement.classList.add("new__tag");
+  newTagElement.textContent = "New";
+
+  const newImgElement = document.createElement("img");
+  newImgElement.alt = new__title;
+  newImgElement.src = imgSrs;
+  newImgElement.classList.add("new__img");
+}
