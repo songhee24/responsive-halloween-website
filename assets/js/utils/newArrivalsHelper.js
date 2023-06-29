469d97723d6df23d5c739fe1844450b78be798e2
songhee24 <akaratel61@gmail.com>
2023-06-30 01:11:11 +0600

    creating newArrivals

---
+++ assets/js/utils/newArrivalsHelper.js
@@ -0,0 +1,44 @@
+const newArrivals = [
+  {
+    new__title: "Haunted House",
+    new__subtitle: "Accessory",
+    imgSrs: "assets/img/new1-img.png",
+    price: "$14.99",
+    discountPrice: "$29.99",
+  },
+  {
+    new__title: "Halloween Candle",
+    new__subtitle: "Accessory",
+    imgSrs: "assets/img/new2-img.png",
+    price: "$14.99",
+    discountPrice: "$29.99",
+  },
+  {
+    new__title: "Witch Hat",
+    new__subtitle: "Accessory",
+    imgSrs: "assets/img/new3-img.png",
+    price: "$14.99",
+    discountPrice: "$29.99",
+  },
+  {
+    new__title: "Rip",
+    new__subtitle: "Accessory",
+    imgSrs: "assets/img/new4-img.png",
+    price: "$14.99",
+    discountPrice: "$29.99",
+  },
+  {
+    new__title: "Terrifying Crystal Ball",
+    new__subtitle: "Accessory",
+    imgSrs: "assets/img/new5-img.png",
+    price: "$14.99",
+    discountPrice: "$29.99",
+  },
+  {
+    new__title: "Witch Broom",
+    new__subtitle: "Accessory",
+    imgSrs: "assets/img/new6-img.png",
+    price: "$14.99",
+    discountPrice: "$29.99",
+  },
+];
