351484aac51f59c41c3d7c2ff1591d2277d30a00
songhee24 <akaratel61@gmail.com>
2023-07-05 04:03:57 +0600

    scrollreveal lib reveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,3 +97,12 @@ function scrollUp() {
 window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // reset: true
+});
+
+sr.reveal(`.home-swiper, .new-swiper, .newsletter__container`);
